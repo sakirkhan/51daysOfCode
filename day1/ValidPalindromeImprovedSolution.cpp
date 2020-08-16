a5fca3a7b8951cc3e76e2cf5fe12fc6c6b1ae2f9
sakir.khan <sakir.khan@jeevansathi.com>
2020-08-16 18:58:53 +0530

    improved solut

---
+++ day1/ValidPalindromeImprovedSolution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+ 
+    
+    bool isPalindrome(string s) {
+        int len = s.length();
+        if(len == 0)
+            return true;
+        int i=0,j=len-1;
+        while(i <= j)
+        {
+            if(!isalnum(s[i])){
+                i++;
+            }
+                
+            else if(!isalnum(s[j])){
+                 j--;
+            }
+               
+             else if(tolower(s[i])!=tolower(s[j])){
+                 return false;
+                
+             }
+                 
+             else{
+                   i++;j--;  
+             } 
+                   
+         }
+        return true;
+    }
+    
+    
+};
